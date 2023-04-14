b9252d252c7cb66a67c345fde5cf5b8efd080113
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-04-14 20:14:35 -0300

    se contruye la logica del cronometro

--- js/apps.js
+++ js/apps.js
@@ -4,30 +4,71 @@ let parrafoSegundos = document.querySelector(`#segundos`);
 let btnStart = document.querySelector(`#btnPlay`)
 let btnPause = document.querySelector(`#btnPause`)
 let btnReset = document.querySelector(`#btnReset`)
+let horas = 0
+let minutos = 0
+let segundos = 0
 
 btnStart.addEventListener(`click`, empezar);
-btnPause.addEventListener(`click`, pararSeguir);
+btnPause.addEventListener(`click`, parar);
 btnReset.addEventListener(`click`, reiniciar);
 
 function empezar(){
+    control = setInterval(controlTiempo, 1000)
     btnStart.disabled = true;
     btnPause.disabled = false;
     btnReset.disabled = false;
 }
 
-function pararSeguir(){
-    btnStart.disabled = true
-    btnPause.disabled = false
+function parar(){
+    clearInterval(control)
+    btnStart.disabled = false
+    btnPause.disabled = true
     btnReset.disabled = false
 }
 
 function reiniciar(){
+    clearInterval(control)
     parrafoHoras.innerHTML = `00`
     parrafoMinutos.innerHTML = `: 00`
     parrafoSegundos.innerHTML = `: 00`
+    horas = 0
+    minutos = 0
+    segundos = 0
     btnStart.disabled = false
     btnPause.disabled = true
     btnReset.disabled = true
 }
+
 function controlTiempo(){
+    if(segundos < 59){
+        segundos++
+        if(segundos < 10){
+            parrafoSegundos.innerHTML =`: 0${segundos}` 
+        }else{
+            parrafoSegundos.innerHTML = `: ${segundos}` 
+        } 
+    } 
+    if(segundos == 59){
+        segundos = -1
+    }
+    if (segundos == 0){
+        minutos++
+        if(minutos < 10){
+            parrafoMinutos.innerHTML =`: 0${minutos}`
+        } else{
+            parrafoMinutos.innerHTML = `:${minutos}`
+        }
+    }
+    if(minutos == 59){
+        minutos = -1
+    }
+    if((segundos == 0) && (minutos == 0)){
+        horas++
+        if(horas < 10){
+            parrafoHoras.innerHTML = `0${horas}`
+        } else{
+            parrafoHoras.innerHTML = `${horas}`
+        }
+    }
+    
 }
